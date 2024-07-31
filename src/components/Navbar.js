85a474e79e69c175a6822e877d21c521823359c0
adityame <aditya300100@gmail.com>
2024-07-31 12:54:27 -0400

    updated favicon, removed repopulate button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
 // import Link from "next/link";
-import { repopulateDb } from "@/helpers/MaizeyApiCall";
+// import { repopulateDb } from "@/helpers/MaizeyApiCall";
 
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center sticky top-0 z-50 p-4 bg-primary-blue text-white">
       <p className="text-3xl font-primary text-white font-semibold">Risk Intelligence Dashboard</p>
-      <button className="border px-2 py-1 rounded-md" onClick={()=>repopulateDb()}>Repopulate Database</button>
     </nav>
   );
 }
